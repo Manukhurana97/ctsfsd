72fae7eb9edb2667902474e9ae2d151de9b998fc
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2020-01-07 12:15:42 +0530

    autoboxing

---
+++ 07-jan-2019/src/pack/autoboxing.java
@@ -0,0 +1,18 @@
+package pack;
+
+public class autoboxing {
+
+	public static void main(String[] args) {
+		
+		int a = 10; // primitive integer
+		Integer z; // Wrapper
+		Integer i = a; // autoboxing
+		int b =i; // unboxing
+		
+		System.out.println(a);
+		System.out.println(i);
+		System.out.println(b);
+
+	}
+
+}
