e447e0cbafce404dcc5e960c50539378f84f1900
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-06 09:38:29 +0530

    Update User.java

--- DesignPrinciples/DesignPatterns/Builder/User.java
+++ DesignPrinciples/DesignPatterns/Builder/User.java
@@ -1,1 +1,88 @@
+package DesignPattern;
+
+/**
+ * @Builder, @Autowire annotation uses the same principle
+ */
+
+public class User {
+
+    public String name;
+    public int age;
+    public Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    // instance of builder class
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public String toString(){
+        return "name: "+name+" age: "+age+" \nAddress: "+address;
+    }
+
+    // builder class
+    public static class UserBuilder {
+
+        public String name;
+        public int age;
+        public Address address;
+        private User user;
+
+        public UserBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public UserBuilder setAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        // public no ag constructor of parent class
+        public User build() {
+            return this.build(name, age, address);
+        }
+
+        private User build(String name, int age, Address address) {
+             user.setName(name);
+             user.setAge(age);
+             user.setAddress(address);
+            return this.user;
+        }
+
+        public User getUser() {
+            return this.user;
+        }
+
+    }
+
+}
 
