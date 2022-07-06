5265135810ff1e500ad308582d439c0e727a8dff
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-06 09:39:32 +0530

    Create Client.java

---
+++ DesignPrinciples/DesignPatterns/Builder/Client.java
@@ -0,0 +1,14 @@
+package DesignPattern;
+
+public class Client {
+
+    public static void main(String[] args){ 
+        Address address = Address.builder().setStreet("14 avanue").setCity("cleveland").setState("ohio").setCountry("USA").build();
+        User user = User.builder().setName("ABC").setAge(10).setAddress(address).build();
+
+         System.out.println(user.toString());
+     }
+
+    
+    
+}
