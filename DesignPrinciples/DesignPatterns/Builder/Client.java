1e00f341667c8c266a396f1d9112cb344f40a810
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-06 11:02:37 +0530

    Update Client.java

--- DesignPrinciples/DesignPatterns/Builder/Client.java
+++ DesignPrinciples/DesignPatterns/Builder/Client.java
@@ -6,9 +6,6 @@ public class Client {
         Address address = Address.builder().setStreet("14 avanue").setCity("cleveland").setState("ohio").setCountry("USA").build();
         User user = User.builder().setName("ABC").setAge(10).setAddress(address).build();
 
-         System.out.println(user.toString());
+        System.out.println(user.toString());
      }
-
-    
-    
 }
