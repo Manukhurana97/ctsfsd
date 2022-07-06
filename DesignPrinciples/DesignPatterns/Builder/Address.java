8059fc964b0fbae3617846c35ee232a944fb8caf
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-06 09:38:59 +0530

    Create Address.java

---
+++ DesignPrinciples/DesignPatterns/Builder/Address.java
@@ -0,0 +1,93 @@
+package DesignPattern;
+
+public class Address {
+
+    public String city;
+    public String state;
+    public String country;
+    public String street;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public static AddressBuilder builder() {
+        return new AddressBuilder();
+    }
+
+    public String toString() {
+        return "Street: " + street + " City: " + city + " Street: " + street + " Country" + country;
+    }
+
+    public static class AddressBuilder {
+
+        private String city;
+        private String state;
+        private String country;
+        private String street;
+        private Address address;
+
+        public AddressBuilder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public AddressBuilder setState(String state) {
+            this.state = state;
+            return this;
+        }
+
+        public AddressBuilder setCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public AddressBuilder setStreet(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public Address build() {
+            return this.build(street, city, state, country);
+        }
+
+        private Address build(String street, String city, String state, String country) {
+            address.setStreet(street);
+            address.setCity(city);
+            address.setState(state);
+            address.setCountry(country);
+
+            return this.address;
+        }
+
+    }
+
+}
