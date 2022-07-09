092a91d35e9a650200e15aa2cdd492f9edff3eea
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-09 16:10:20 +0530

    Create Message.java

---
+++ DesignPrinciples/DesignPatterns/FactoryMethod/Message.java
@@ -0,0 +1,12 @@
+package DesignPattern;
+
+public abstract class Message {
+
+    public abstract String getContent();
+
+    public void addDefaultMethod() {
+    }
+
+    public void encrypt() {
+    }
+}
