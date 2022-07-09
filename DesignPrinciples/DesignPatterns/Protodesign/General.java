f46b913642f3e7398fc88e89bb68928d9242d1bb
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-09 18:40:06 +0530

    Update General.java

--- DesignPrinciples/DesignPatterns/Protodesign/General.java
+++ DesignPrinciples/DesignPatterns/Protodesign/General.java
@@ -11,6 +11,17 @@ public class General extends GameUnit {
     public String toString() {
         return "General{" +
                 "state='" + state + '\'' +
+                ", = "+getPosition() +
                 '}';
+    }
+
+    @Override
+    protected void reset() {
+        throw new UnsupportedOperationException("general not supported");
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return new CloneNotSupportedException("generals are unique");
     }
 }
