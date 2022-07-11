d3eeb83fed5c40fd478b8b27ad11fe446618d09f
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-11 08:39:21 +0530

    Create EagerSingleton.java

---
+++ DesignPrinciples/DesignPatterns/SingletonPattern/EagerSingleton.java
@@ -0,0 +1,13 @@
+package DesignPattern;
+
+public class EagerSingleton{
+  
+  private EagerSingleton(){}
+  
+  private static final EagerSingleton INSTANCE = new EagerSingleton();
+  
+  public static EagerSingleton getInstance(){
+    return INSTANCE;
+  }
+  
+}
