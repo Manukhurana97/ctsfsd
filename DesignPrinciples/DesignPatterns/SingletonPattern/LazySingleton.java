29df96bebd5567b526d8ffaf906d9a792378b933
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-11 08:58:08 +0530

    Update LazySingleton.java

--- DesignPrinciples/DesignPatterns/SingletonPattern/LazySingleton.java
+++ DesignPrinciples/DesignPatterns/SingletonPattern/LazySingleton.java
@@ -3,17 +3,28 @@ package DesignPattern;
 public class LazySingleton(){
   private static LazySingleton(){}
   
+  
+//   ---------------------------------------------------------------------------------------------
+  
   private static volatile LazySingleton INSTANCE;// to avoid cpu cache 
   
   public static LazySingleton getInstance(){
-    if(INSTANCE==null) {
-      synchronized(LazySingleton.class){
-         if(INSTANCE==null) {
+    if(INSTANCE==null) 
+      synchronized(LazySingleton.class)
+         if(INSTANCE==null) 
           INSTANCE = new LazySingleton();
-         
-         }
-      }
-    }
+ 
     return INSTANCE;
   }
+  
+//   ---------------------------------------------------------------------------------------------
+// another way
+  private static class Registeryholder{
+    static LazySingleton INSTANCE = new LazySingleton();
+  }
+  
+  public static LazySingleton getInstance1(){
+    return Registeryholder.INSTANCE;
+  }
+  
 }
