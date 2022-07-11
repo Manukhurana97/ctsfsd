8860c667b18e1e863c3374672089f1aa6e6eb275
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-11 08:53:41 +0530

    Update Client.java

--- DesignPrinciples/DesignPatterns/SingletonPattern/Client.java
+++ DesignPrinciples/DesignPatterns/SingletonPattern/Client.java
@@ -4,8 +4,12 @@ public class Client{
   public static void main(String args[]){
     EagerSingleton registery = EagerSingleton.getInstance();
     EagerSingleton registery2 = EagerSingleton.getInstance();
+      System.out.println(registery == registery2);
     
-    System.out.println(registery == registery2);
+    LazySingleton lregistery = LazySingleton.getInstance();
+    LazySingleton lregistery2 = LazySingleton.getInstance();
+    
+    System.out.println(lregistery == lregistery2);
   }
 }
   
