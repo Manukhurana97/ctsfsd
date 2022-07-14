c0e89626a502bc53d32767367482e26c03b2550c
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-14 17:58:12 +0530

    Create Queue.java

---
+++ DesignPrinciples/DesignPatterns/Bridge/Queue.java
@@ -0,0 +1,20 @@
+package DesignPattern;
+
+public class Queue<T> implements FifoCollections<T> {
+
+    private LinkedList<T> list;
+
+    public Queue(LinkedList<T> list){
+        this.list = list;
+    }
+
+    @Override
+    public void offer(T element) {
+        list.addLast(element);
+    }
+
+    @Override
+    public T poll() {
+        return list.removeFirst();
+    }
+}
