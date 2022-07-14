37b14c2dbdc4c745e855a323b6834f07d1633c84
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2022-07-14 07:58:57 +0530

    Create ICustomer.java

---
+++ DesignPrinciples/DesignPatterns/Adaptive/ICustomer.java
@@ -0,0 +1,14 @@
+package DesignPattern;
+
+/**
+ * Target interface that require by new client code
+ * Target interface
+ * */
+public interface ICustomer {
+
+    String getName();
+
+    String getTitle();
+
+    String getEmail();
+}
